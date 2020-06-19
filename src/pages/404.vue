10492211e46affc5b4f5677b23bb89d6c44831e8
Brad <bradsiefert@icloud.com>
2020-06-18 20:59:36 -0500

    404 stuff

---
+++ src/pages/404.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <Layout 
+    heroTitle="Oh no! 404 Error" 
+    heroSubtitle="Looks like you've found a bad link."
+  >
+  <section class="section content">
+    <div class="container tiny">
+      <hr>
+      <div class="title is-4">Page Not Found</div>
+      We're sorry about this. If you'd let me know about this on 
+      <a target="_blank" href="https://twitter.com/bksiefert">Twitter</a>, I'd appreciate it. Have a good day.
+    </div>
+  </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
